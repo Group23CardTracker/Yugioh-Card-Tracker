0faf76ba62755a19dae1bd82ceaf1ec69e01cfbc
thorn-t <thornt@sunypoly.edu>
2022-10-29 14:59:20 -0400

    added fragments

--- app/src/main/java/com/example/yu_gi_ohcardtracker/MainActivity.kt
+++ app/src/main/java/com/example/yu_gi_ohcardtracker/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.yu_gi_ohcardtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define your fragments here
+        val fragment1: Fragment = Home()
+        val fragment2: Fragment = BanList()
+        val fragment3: Fragment = NewCards()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.action_home -> fragment = fragment1
+                R.id.action_banlist -> fragment = fragment2
+                R.id.action_newcards -> fragment = fragment3
+            }
+            fragmentManager.beginTransaction().replace(R.id.rlContainer, fragment).commit()
+            true
+        }
+
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.action_home
     }
+
 }
